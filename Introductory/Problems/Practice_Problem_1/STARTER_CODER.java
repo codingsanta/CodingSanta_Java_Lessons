379680994291fcfa9f1735a780a10ef822c047b8
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-04-05 21:40:30 -0700

    Create STARTER_CODER.java

---
+++ Introductory/Problems/Practice_Problem_1/STARTER_CODER.java
@@ -0,0 +1,27 @@
+/*
+ * This file contains the starter code and the sample input for reference
+ */
+public class Students
+{
+  public static void main(String [] args)
+  {
+    
+  }
+}
+
+/*
+
+    SAMPLE input & output
+-----------------------------------------------------------------
+  How many students are in Mr. James’s class?
+  $: 17
+  He can make 5 full groups and there are 2 remaining students.
+-----------------------------------------------------------------
+  How many students are in Mr. James’s class?
+  $: 30
+  He can make 10 full groups and there are 0 remaining students.
+-----------------------------------------------------------------
+
+  Problem Credits: Lance Fuchia
+  
+*/
