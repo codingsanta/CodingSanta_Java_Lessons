4747acadd9a4a94112a9eb80d8cbac3834d2325f
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-04-13 23:19:21 -0700

    Create Test.java

---
+++ Introductory/Notes/Commands/Test.java
@@ -0,0 +1,28 @@
+public class Test
+{
+	public static void main(String [] args)
+	{
+		System.out.println("This is a working program.");
+	}
+}
+
+/*
+
+COMMANDS
+
+javac fileName.java -> compiles the program
+java fileName		-> executes the program
+
+pwd					-> prints current directory
+cd directoryName	-> changes current directory to directoryName
+cd					-> changes into the root directory
+cd ..				-> moves "back" one directory
+ls					-> prints files in current directory
+
+clear				-> clears the window of the console
+arrow keys			-> navigate between commands
+touch newFile.java	-> creates a java file inside current directory
+mkdir directoryName -> creates a directory inside current directory
+cat fileName.java 	-> prints the content of fileName.java
+
+*/
