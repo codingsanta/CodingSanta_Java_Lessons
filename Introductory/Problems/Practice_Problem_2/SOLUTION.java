0cc8b2e2706148fb8183b608d01146b57e88d3ab
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-04-16 11:18:16 -0700

    .

---
+++ Introductory/Problems/Practice_Problem_2/SOLUTION.java
@@ -0,0 +1,52 @@
+/*
+ * Here is the solution code. Change the file to
+ * "Password.java" if you plan to run the program.
+ */
+
+import java.util.Scanner;
+
+public class PP2
+{
+	public static void main(String [] args)
+	{
+		System.out.println();
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter the key for the first code: ");
+		int firstKey = keyboard.nextInt();
+		System.out.print("Enter the key for the second code: ");
+		double secondKey = keyboard.nextDouble();
+		System.out.print("Enter the key for the third code: ");
+		int thirdKey = keyboard.nextInt();
+		System.out.print("Enter the key for the fourth code: ");
+		String fourthKey = keyboard.next();
+		System.out.print("Enter the encryption pin: ");
+		int encryptionPin = keyboard.nextInt();
+		System.out.println();
+
+		System.out.print("Passcode: ");
+		System.out.print(firstCode(firstKey) + " ");
+		System.out.print(secondCode(secondKey) + " ");
+		System.out.print(thirdCode(thirdKey) + " ");
+		System.out.print(fourthCode(fourthKey, encryptionPin));
+		System.out.println();
+
+		System.out.println();
+	}
+	public static int firstCode(int key)
+	{
+		return key + key * key;
+	}
+	public static double secondCode(double key)
+	{
+		return key * (key - 1.0) * (key - 2.0);
+	}
+	public static int thirdCode(int key)
+	{
+		return key % 4;
+	}
+	public static String fourthCode(String name, int encryption)
+	{
+		return name + encryption;
+	}
+}
