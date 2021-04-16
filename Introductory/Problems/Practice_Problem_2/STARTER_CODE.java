85deba767066277b278dc8f650af36a71b204da6
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-04-16 11:16:23 -0700

    .

---
+++ Introductory/Problems/Practice_Problem_2/STARTER_CODE.java
@@ -0,0 +1,18 @@
+/* 
+ * Here is the starter code. Be sure to change the file
+ * name to "Password.java" when you make the program!
+ */
+public class Password
+{
+  public static void main(String [] args)
+  {
+    
+  }
+  // methods go below
+  
+  // sample method declaration
+  public static int firstCode(int key)
+  {
+    
+  }
+}
