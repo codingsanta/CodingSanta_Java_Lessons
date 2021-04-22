bb5f1a052d4dffc9705773f3b9286fd0a5011ed8
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-04-21 21:19:13 -0700

    .

--- Introductory/Notes/5_Input_Output/InputOutput.java
+++ Introductory/Notes/5_Input_Output/InputOutput.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+public class InputOutput
+{
+  public static void main (String [] args)
+  {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Please input a number: ");
+      int number = keyboard.nextInt();
+      System.out.println();
+      System.out.println(number);
 
+      System.out.print("Please input a decimal: ");
+      double decimal = keyboard.nextDouble();
+      System.out.println();
+      System.out.println(decimal);
+
+      System.out.print("Please enter a string: ");
+      String input = keyboard.next();
+       System.out.println();
+      System.out.println(input);
+
+      System.out.println("Please enter a line of text: ");
+      String line = keyboard.nextLine();
+      System.out.println();
+       System.out.println(line);
+  }
+}
