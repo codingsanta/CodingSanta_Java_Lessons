de48ccb864b1620ac05ea7858fe97613f1740db4
codingsanta <81771486+codingsanta@users.noreply.github.com>
2021-05-03 21:07:24 -0700

    Update Main.java

--- Introductory/Notes/7_OOP_Classes_And_Objects/Main.java
+++ Introductory/Notes/7_OOP_Classes_And_Objects/Main.java
@@ -1,1 +1,11 @@
-// code here
+public class Main
+{
+  int x = 5;
+    public static void main (String[]args)
+    {
+      Main myObject = new Main();
+      Main myObjectTwo = new Main();
+      System.out.println(myObjectTwo.x);
+    }
+}
+
